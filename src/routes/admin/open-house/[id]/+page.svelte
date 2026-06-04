0f80f73be1539d141d4b3e6db2629c4348b5f02d
Inmublia <admin@inmublia.com>
2026-06-04 08:31:46 -0600

    Create +page.svelte

---
+++ src/routes/admin/open-house/[id]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import OpenHouse from '$lib/components/OpenHouse.svelte';
+  // agentMode={true} le dice al componente que muestre tu panel de control con métricas
+</script>
+
+<OpenHouse agentMode={true} />
