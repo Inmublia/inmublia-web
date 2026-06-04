d39ad396416f1cb1a29755dc293d3d6db6785e7f
Inmublia <admin@inmublia.com>
2026-06-04 08:30:55 -0600

    Create +page.svelte

---
+++ src/routes/open-house/[id]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import OpenHouse from '$lib/components/OpenHouse.svelte';
+  // agentMode={false} le dice al componente que muestre la vista de RSVP para clientes
+</script>
+
+<OpenHouse agentMode={false} />
