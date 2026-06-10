410c1860517e745754b3d2336cc8b44b856430d6
Inmublia <admin@inmublia.com>
2026-06-09 23:49:33 -0600

    Create +layout.svelte

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Sidebar from '$lib/components/Sidebar.svelte';
+  let { children } = $props();
+</script>
+
+<div class="min-h-screen bg-zinc-50 flex font-sans text-slate-900 selection:bg-blue-100 overflow-hidden relative">
+  <Sidebar />
+  
+  {@render children()}
+</div>
