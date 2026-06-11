947155f46d1739b80d96f1c0ea0d34e77bcd0e65
Inmublia <admin@inmublia.com>
2026-06-10 23:01:39 -0600

    Update +page.svelte

--- src/routes/admin/open-house/[id]/+page.svelte
+++ src/routes/admin/open-house/[id]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
-  // Asumiendo que el archivo de admin se quedó como OpenHouse.svelte o OpenHouseAdmin.svelte
   import OpenHouseAdmin from '$lib/components/OpenHouse.svelte'; 
+  
   let { data } = $props();
 </script>
+
 <OpenHouseAdmin event={data.event} attendeesDb={data.attendeesDb} agentMode={true} />
